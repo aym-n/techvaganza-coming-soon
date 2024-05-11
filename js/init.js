e62488c745086b847226dad1501b77ec03a394ef
aym-n <ajm8982@gmail.com>
2024-05-11 15:47:57 +0530

    🖌️ Update Design

--- js/init.js
+++ js/init.js
@@ -1,9 +1,7 @@
-//Hook up the tweet display
-
 $(document).ready(function() {
 						   
 	$(".countdown").countdown({
-				date: "16 dec 2018 14:30:00",
+				date: "7 jun 2024 8:30:00",
 				format: "on"
 			},
 			
